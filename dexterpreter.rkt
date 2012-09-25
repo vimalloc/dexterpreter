2946580cf4e1e1d1b10a8f93d1a68c32d7f75a74
Andrew Kuhnhausen <trane@xmission.com>
2012-09-24 21:58:49 -0600

    Implement a completely useless struct that is wrong

--- dexterpreter.rkt
+++ dexterpreter.rkt
@@ -5,3 +5,14 @@
 ;   continuation: a seq of frames denoting procedure return contexts and
 ;                 exception handlers for this machine (kont)
 (struct state {stmts fp store kont})
+
+; class-def ::= class class-name extends class-name
+;               { field-def ... method-def ... }
+; attrs: class_def_item
+;   access_flags, superclass_idx, interfaces_off, source_file_idx,
+;   annotation_off, class_data_off, static_values_off
+(struct class-def { fields methods name super attrs })
+
+; field-def ::= var field-name ;
+; method-def ::= def method-name($name, ..., $name) { body }
+
