e8279ba2fd660dfd106d135b6840274cb290cb0e
Andrew Kuhnhausen <trane@xmission.com>
2012-09-25 20:40:39 -0600

    Start a match-expander for invoke-*

--- dexterpreter.rkt
+++ dexterpreter.rkt
@@ -6,13 +6,13 @@
 ;                 exception handlers for this machine (kont)
 (struct state {stmts fp store kont})
 
-; class-def ::= class class-name extends class-name
-;               { field-def ... method-def ... }
-; attrs: class_def_item
-;   access_flags, superclass_idx, interfaces_off, source_file_idx,
-;   annotation_off, class_data_off, static_values_off
-(struct class-def { fields methods name super attrs })
 
-; field-def ::= var field-name ;
-; method-def ::= def method-name($name, ..., $name) { body }
-
+; invoke-{virtual,super,direct,static,interface} {v1, ...} method-to-call
+(define-match-expander invoke
+  (lambda stx
+    (syntax-case stx ()
+      ; invoke-virtual
+      ; (invoke-virtual {v3 v0 v1 v2} method v0-type [v1-type] v2-type
+      ;  => invoke virtual method with params v0..2, v3 is 'this'
+      [(invoke-virtual {,this . ,params} ,meth . ,types) stx]
+      [(invoke-super {})]))
