ad196df70cc831a8f63021863f020812060a18cd
Andrew Kuhnhausen <trane@xmission.com>
2012-09-25 21:22:56 -0600

    Add a list of what we'll need to write matches for

--- dexterpreter.rkt
+++ dexterpreter.rkt
@@ -14,10 +14,38 @@
       ; invoke-virtual
       ; (invoke-virtual {v3 v0 v1 v2} method v0-type [v1-type] v2-type
       ;  => invoke virtual method with params v0..2, v3 is 'this'
-      [(invoke-virtual {,this . ,params} ,meth . ,types) stx]
+      [`(invoke-virtual {,this . ,params} ,meth . ,types) stx]
       ; invoke-super
       ; (invoke-super {v1 v2} method v2-type
       ;  => invoke the method from the super class with arguments v1 and v2
-      [(invoke-super {,args} ,meth . ,types) stx]
+      [`(invoke-super {,args} ,meth . ,types) stx]
       ; invoke-direct {v0} method
-      [(invoke-direct {,this} ,meth) stx])))
+      [`(invoke-direct {,this} ,meth) stx])))
+
+; Opcodes we care about (in order):
+; http://pallergabor.uw.hu/androidblog/dalvik_opcodes.html
+
+; move
+; return
+; const
+; monitor
+; switch
+; instance-of
+; array-length
+; new-array
+; throw
+; goto
+; switch
+; compare
+; conditions
+; array get
+; array put
+; instance get
+; instance put
+; static get
+; static put
+; invoke
+; conversion
+; arithmetic operations
+; arithmetic and store
+; *-quick
